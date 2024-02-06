2681ec5101ded8df50f6cdd945eff14ab13b6ef2
David Gómez <dagove47@gmail.com>
2024-02-06 01:03:51 -0600

    Form validation added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,46 @@ window.addEventListener("load", () => {
     loader.style.display = "none";
 })
 
+/* 
+    Purchase
+*/
+
+const quotation_form = document.getElementById('quotation_form');
+const submitBtn = document.getElementById('submitBtn');
+const allPurchases = [];
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation()
+    if (!quotation_form.checkValidity()) {
+        quotation_form.classList.add('was-validated');
+    } else {
+        console.log("test");  
+    }
+    
+});
+
+class Purchase {
+    constructor(item, price, amount, token) {
+        this.item = item;
+        this.price = price;
+        this.amount = amount
+        this.token = token;
+    }
+}
+
+function addPurchase(item, price, amount) {
+    const newPurchase = new Purchase(item, price, amount);
+    allPurchases.push(newPurchase);
+}
+
+
+
+
+
+
+
+
 
 /* 
     PDF
@@ -19,10 +59,20 @@ function generatePDF() {
     doc.save("new File.pdf");
 }
 
+/* 
+    Token
+*/
+function tokenId() {
+    let newToken = '';
+    let tokenCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let tokenLength = 15;
 
+    do {
+        let tokenChar = tokenCharacters.charAt(Math.floor(Math.random() * tokenCharacters.length));
+        newToken += tokenChar;
+        tokenLength--; 
+    }
+    while(tokenLength > 0);
 
-
-
-
-
-
+    return newToken;
+}
